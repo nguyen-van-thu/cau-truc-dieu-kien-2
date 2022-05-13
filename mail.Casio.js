d101dd0351c49643e935913c58f9ad9769dacb57
nguyen-van-thu <102147814+nguyen-van-thu@users.noreply.github.com>
2022-05-13 09:54:11 +0700

    cau-truc-dieu-kien2

---
+++ mail.Casio.js
@@ -0,0 +1,3 @@
+function total() {
+let toan = document.getElementById("input").value;
+}
---
+++ tinhtoan.js
@@ -0,0 +1,10 @@
+let tong = document.getElementById("dong1").value;
+let tong2 = document.getElementById("dong2").value;
+function toan1() {
+    document.getElementById("cong1");
+    result = tong + tong2
+
+
+}
+toan1();
+console.log(toan1());
